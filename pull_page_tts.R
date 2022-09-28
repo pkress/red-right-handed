66b81c4f7466db9088432f7d452114c9ca4fef36
Peter Kress <pkress@Peters-MacBook.local>
2022-09-27 22:38:19 -0500

    feat-ip: use GCP to TTS rrh text

--- pull_page_tts.R
+++ pull_page_tts.R
@@ -17,7 +17,7 @@ setwd("..")
 if(!require("pacman")) install.packages("pacman")
 library(pacman)
 p_load(data.table, magrittr, stringr, ggplot2
-       , RSelenium, rvest)
+       , googleLanguageR)
 
 ## |- Handy Functions ----
 `%p%` = paste0
@@ -29,35 +29,19 @@ month_diff = function(d1, d2){
 make_title = function(x){ str_to_title(gsub("_", " ", x))}
 
 ## |- Set API key ----
-
-tts_api = Sys.getenv("tts_api")
+gl_auth("./red_right_handed/gcp_tts_rrh_api_auth.json")
 
 ##################)
 # | Read in Data ----
 ##################)
 
-rrh_data =  fread("Documents/personal_projects/red_right_handed/red_hand_files.csv")
+rrh_data =  fread("./red_right_handed/red_hand_files.csv")
 
 ##################)
 # | Get Mp3 ----
 ##################)
 
-aa = rvest::session("https://ttstool.com/")
-
-### Best option is probably free tier for Polly from AWS
-
-base_url = "https://api.voicerss.org/"
-params = c(hl = "en-us", key = tts_api, c = "MP3", r = "2", v = "john"
-           , f = "48khz_16bit_stereo")
-payload = rrh_data[1, text]
-download_url = paste0(base_url, "?"
-                      , paste(names(params)%p%"="%p%params, collapse = "&")
-                      , "&src=", payload)
-download.file("https://api.voicerss.org/?key=d5d2a220d838435c99bc4edf48bdccba&hl=en-us&c=MP3&r=2&v=john&f=48khz_16bit_stereo&src=Hello,%20world!
-  
- ")
-rD <- RSelenium::rsDriver(
-  port = 4444L,
-  browser = c("firefox"),
-  version = "latest"
-)
+make_mp3 = function(d){
+  gl_talk(d)
+}
+gl_talk(rrh_data)
