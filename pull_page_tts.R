32047a03caebd936cc374b70c6515ce329ce056e
Peter Kress <pkress@Peters-MacBook.local>
2022-09-27 23:33:35 -0500

    feat: create an mp3 file with title, Qs and As from any rrh file

--- pull_page_tts.R
+++ pull_page_tts.R
@@ -41,7 +41,44 @@ rrh_data =  fread("./red_right_handed/red_hand_files.csv")
 # | Get Mp3 ----
 ##################)
 
-make_mp3 = function(d){
-  gl_talk(d)
+make_audio = function(d, outdir){
+  intro = paste(paste0("Issue number ", d$issue_num
+                               , " published on ", d$issue_month)
+                , paste0("Written by ", gsub(", (?=[A-Z']+$)", ", and "
+                                             , gsub("; ", ", ", d$from_names)
+                                             , perl = T))
+                , paste0("Questions: "
+                         , gsub("\n\nNext Question\n\n"
+                                , " \n\n Next Question. \n\n"
+                                , d$questions))
+                , sep = ".\n")
+  gl_talk(intro, outdir%p%"/intro.mp3", audioEncoding = 'MP3'
+          , name = "en-GB-Wavenet-D", speakingRate = 1, pitch = -2)
+  
 }
-gl_talk(rrh_data)
+
+make_answers = function(d, outdir){
+  response = "Responses from Nick. \n\n"%p%d$text
+  resp_len = nchar(response)
+  rounds = ceiling(resp_len/5000L)
+  lapply(1:rounds, \(x){
+    gl_talk(substr(response, 5000 * (x-1) + 1, 5000*x)
+            , outdir%p%"/response_"%p%x%p%".mp3", audioEncoding = 'MP3'
+            , name = "en-GB-Wavenet-D", speakingRate = 1, pitch = -2)
+    })
+  
+}
+
+make_mp3 = function(d){
+  outdir = paste0("./red_right_handed/", d$issue_num, "_"
+                  , gsub("https://www.theredhandfiles.com/", "", d$title, fixed = T))
+  
+  dir.create(outdir)
+  
+  make_intro(d, outdir)
+  make_answers(d, outdir)
+  
+  system(paste0("cat ", outdir, "/*.mp3 > ", outdir, "/full.mp3"))
+  
+}
+make_mp3(rrh_data[11])
